892b36440d180ed41b4f0fd832d5c5f4865e206f
subharadhe1234 <subhadippratihar09@gmail.com>
2025-02-11 00:43:16 +0530

    demmo

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/')
+def index():
+    return jsonify({"message": "Flask app deployed successfully on Vercel!"})
+
+@app.route('/hello')
+def hello():
+    return jsonify({"message": "Hello from Flask!"})
+
+def handler(event, context):
+    return app(event, context)
+
+if __name__ == "__main__":
+    app.run(debug=True)
